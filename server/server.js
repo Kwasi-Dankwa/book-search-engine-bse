8c084eac2892e392e1d63b9f7a7107d547bd6b42
Kwasi Dankwa <edankwa14@gmail.com>
2022-07-23 16:24:07 -0400

    updating auth middleware and creating schema

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
+const { ApolloServer } = require("apollo-server-express");
+const { authMiddleware } = require("./utils/auth");
+const { typeDefs, resolvers } = require("./schemas");
 const routes = require('./routes');
 
 const app = express();
