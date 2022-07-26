cfb10453d378723b98671198dd725492cfec8be5
Kwasi Dankwa <edankwa14@gmail.com>
2022-07-25 23:58:17 -0400

    pushing latest changes trying to fix saved books functionality

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+import React from "react";
+import {Jumbotron, Container, CardColumns, Card, Button,} from "react-bootstrap";
 
 import { useQuery, useMutation } from "@apollo/react-hooks";
 import { GET_ME } from "../utils/queries";
